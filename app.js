310cca7c8690e33ea7bb7d10e050e2ea13ceae02
Валерий Кулаков <kulakov.angel@gmail.com>
2021-02-22 21:15:47 +0300

    Redy for production

--- app.js
+++ app.js
@@ -1,3 +1,62 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = new Sequelize(
+    'test-test',
+    'root',
+    'root',
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        logging: false,
+    }
+);
+
+class User extends Model {}
+class ToDo extends Model {}
+
+User.init({
+    name: DataTypes.STRING,
+    age: DataTypes.INTEGER,
+}, { sequelize });
+
+
+ToDo.init({
+    title: DataTypes.STRING,
+}, { sequelize });
+
+User.hasMany(ToDo);
+ToDo.belongsTo(User);
+User.sync();
+ToDo.sync();
+
+app.get('/', (req, res, next) => {
+    res.send('Hello, world!');
+});
+
+app.get('/users', async (req, res, next) => {
+    const users = await User.findAll();
+    res.json(users);
+});
+
+app.get('/users/:id', async (req, res, next) => {
+    const userById = await User.findByPk(req.params.id, {include: 'ToDos'});
+    res.json(userById);
+});
+
+app.post('/users', async (req, res, next) => {
+    const user = await User.create({
+        name: req.body.name,
+        age: +req.body.age,
+    });
+    const toDo = await ToDo.create({title: 'Hello'});
+    await toDo.setUser(user);
+    await toDo.save();
+    res.status(201).json(user);
+});
+
+app.listen(3000);
 
 module.exports = app;
--- app.test.js
+++ app.test.js
@@ -1,28 +1,45 @@
 const request = require('supertest');
 const app = require('./app');
+const should = require('should');
 
 describe('api', function () {
     it('should return "Hello, world!" on GET /', function(done) {
         request(app)
             .get('/')
             .expect(200)
-            .expect('Hello, world!')
+            .expect(function (response) {
+                response.text.toLowerCase().should.equal('Hello, world!'.toLowerCase())
+            })
             .end(done);
     });
 
-    it('should return user on GET /user', function(done) {
+    it('should return ALL users on GET /users', function(done) {
         request(app)
-            .get('/user')
+            .get('/users')
             .expect(200)
             .expect(function (response) {
-                //todo: add should library
-                require('asset').deepEqual(
-                    response.body,
-                    {
-                        name: 'Ivan',
-                        age: 18,
-                    }
-                )
+                response.body.should.be.an.Array();
+            })
+            .end(done);
+    });
+
+    // it('should return user by id on GET /users/:id', function(done) {
+    //     request(app)
+    //         .get('/users/5')
+    //         .expect(200)
+    //         .expect(function (response) {
+    //             response.body.should.have.keys('name', 'age', 'id');
+    //         })
+    //         .end(done);
+    // });
+
+    it('should return user on POST /user', function(done) {
+        request(app)
+            .post('/users')
+            .send({name: 'Ivan', age: 30})
+            .expect(201)
+            .expect(function (response) {
+                response.body.should.have.keys('name', 'age', 'id');
             })
             .end(done);
     });
